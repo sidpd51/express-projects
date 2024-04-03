2d3a4e488ab8f4698b6657764ab14cb95ec92221
siddharthpd <sidpd510@gmail.com>
2024-04-03 18:36:12 +0530

    final task-manager

--- task-manager/app.js
+++ task-manager/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+require('dotenv').config()
+
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
-require('dotenv').config()
 
 //middleware
 app.use(express.static('./public'))
